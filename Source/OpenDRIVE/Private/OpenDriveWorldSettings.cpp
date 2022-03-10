0944073a8519ac4edbff6d55919dcf399a31c836
Bertrand Richard <bertrand.richard@univ-eiffel.fr>
2022-03-10 18:09:45 +0100

    Fixed ODR not reloaded if current asset was reimported

--- Source/OpenDRIVE/Private/OpenDriveWorldSettings.cpp
+++ Source/OpenDRIVE/Private/OpenDriveWorldSettings.cpp
@@ -3,7 +3,13 @@
 
 #include "OpenDriveWorldSettings.h"
 #include "Misc/UObjectToken.h"
+#include "Editor/EditorEngine.h"
+#include "Subsystems/ImportSubsystem.h"
+#include "Logging/MessageLog.h"
 #include "Roadmanager.hpp"
+#if WITH_EDITOR
+#include "Editor.h"
+#endif
 
 #if WITH_EDITOR
 void AOpenDriveWorldSettings::PostEditChangeProperty(FPropertyChangedEvent& e) {
@@ -38,13 +44,29 @@ void AOpenDriveWorldSettings::LoadOpenDrive() {
 	if (!OpenDriveAsset) return;
 	if (GetLevel() && !GetLevel()->IsPersistentLevel()) {
 		UE_LOG(OpenDriveLog, Warning, TEXT("%s is a streamed level but has an OpenDRIVE set, its OpenDRIVE will be ignored"), *(GetFName().ToString()));
+		return;
 	}
 	if (!roadmanager::Position::GetOpenDrive()->LoadOpenDriveContent(TCHAR_TO_UTF8(*(OpenDriveAsset->XodrContent)))) {
 		UE_LOG(OpenDriveLog, Error, TEXT("Failed to load OpenDRIVE asset %s"), *(OpenDriveAsset->GetFName().ToString()));
+		return;
 	}
+#if WITH_EDITOR
+	if (GEditor && !bRegisteredReimportCallback) {
+		GEditor->GetEditorSubsystem<UImportSubsystem>()->OnAssetReimport.AddUObject(this, &AOpenDriveWorldSettings::OnObjectReimported);
+		bRegisteredReimportCallback = true;
+	}
+#endif
 }
 
 void AOpenDriveWorldSettings::PostLoad() {
 	Super::PostLoad();
 	LoadOpenDrive();
 }
+
+#if WITH_EDITOR
+void AOpenDriveWorldSettings::OnObjectReimported(UObject* InObject) {
+	if (InObject == OpenDriveAsset) {
+		LoadOpenDrive();
+	}
+}
+#endif
