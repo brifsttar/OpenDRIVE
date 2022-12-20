46154533e8759ebc22c9a9d458037c0fb8690fcf
Bertrand Richard <bertrand.richard@ifsttar.fr>
2022-12-20 08:05:33 +0100

    Added OpenDrivePosition

---
+++ Source/OpenDRIVE/Private/OpenDrivePosition.cpp
@@ -0,0 +1,140 @@
+#include "OpenDrivePosition.h"
+
+UOpenDrivePosition::UOpenDrivePosition() {
+	_TrackPos.SetSnapLaneTypes(roadmanager::Lane::LaneType::LANE_TYPE_ANY);
+}
+
+void UOpenDrivePosition::InvalidateCache() {
+	_InertialPosCache = FTransform();
+}
+
+void UOpenDrivePosition::SetTrackPosition(const roadmanager::Position& p) {
+	InvalidateCache();
+	_TrackPos = p;
+	_TrackPos.SetSnapLaneTypes(roadmanager::Lane::LaneType::LANE_TYPE_ANY);
+}
+
+roadmanager::Position UOpenDrivePosition::OdrPosition() const {
+	return _TrackPos;
+}
+
+void UOpenDrivePosition::SetTransform(const FTransform& T) {
+	if (T.Equals(_InertialPosCache)) return;
+	_InertialPosCache = T;
+	FVector p = CoordTranslate::UeToOdr::Location(T.GetLocation());
+	FVector r = CoordTranslate::UeToOdr::Rotation(T.Rotator().Euler());
+	_TrackPos.SetInertiaPos(p.X, p.Y, p.Z, r.X, r.Y, r.Z);
+}
+
+FTransform UOpenDrivePosition::GetTransform() const {
+	return CoordTranslate::OdrToUe::ToTransfrom(_TrackPos);
+}
+
+void UOpenDrivePosition::SetTrackPosition(int TrackId, int LaneId, float S, float Offset, float H) {
+	roadmanager::Position p(TrackId, LaneId, UuToMeters(S), UuToMeters(Offset));
+	p.SetHeadingRelative(FMath::DegreesToRadians(H));
+	SetTrackPosition(p);
+}
+
+bool UOpenDrivePosition::MoveAlongS(float S, int Strategy) {
+	roadmanager::Position p = OdrPosition();
+	roadmanager::Position::ReturnCode ret;
+	ret = p.MoveAlongS(UuToMeters(S), 0., roadmanager::Junction::JunctionStrategyType(Strategy));
+	SetTrackPosition(p);
+	return ret >= roadmanager::Position::ReturnCode::OK;
+}
+
+void UOpenDrivePosition::ResetPosition() {
+	SetTrackPosition(roadmanager::Position());
+}
+
+int UOpenDrivePosition::GetRoadId() const {
+	return OdrPosition().GetTrackId();
+}
+
+int UOpenDrivePosition::GetLaneId() const {
+	return OdrPosition().GetLaneId();
+}
+
+float UOpenDrivePosition::GetS() const {
+	return MetersToUu(OdrPosition().GetS());
+}
+
+float UOpenDrivePosition::GetT() const {
+	return MetersToUu(OdrPosition().GetOffset());
+}
+
+void UOpenDrivePosition::SetT(float T) {
+	roadmanager::Position p = OdrPosition();
+	p.SetOffset(UuToMeters(T));
+	SetTrackPosition(p);
+}
+
+float UOpenDrivePosition::GetH() const {
+	return FMath::RadiansToDegrees(OdrPosition().GetHRelativeDrivingDirection());
+}
+
+void UOpenDrivePosition::SetH(float H) {
+	roadmanager::Position p = OdrPosition();
+	p.SetHeadingRelative(FMath::DegreesToRadians(H));
+	SetTrackPosition(p);
+}
+
+float UOpenDrivePosition::GetLaneWidth() const {
+	roadmanager::Position p = OdrPosition();
+	double s = p.GetS();
+	const roadmanager::Road* r = p.GetRoad();
+	if (r == nullptr) return NAN;
+	const roadmanager::LaneSection* ls = r->GetLaneSectionByS(s);
+	if (ls == nullptr) return NAN;
+	return MetersToUu(ls->GetWidth(s, p.GetLaneId()));
+}
+
+int UOpenDrivePosition::GetJunctionId() const {
+	roadmanager::Road* r = OdrPosition().GetRoad();
+	return r ? r->GetJunction() : -1;
+}
+
+float UOpenDrivePosition::GetNextJunctionDistance() const {
+	return MetersToUu(OdrPosition().GetNextJunction().distance);
+}
+
+int UOpenDrivePosition::GetNextJunctionId() const {
+	return OdrPosition().GetNextJunction().junction->GetId();
+}
+
+bool UOpenDrivePosition::IsJunctionDistanceLessThan(float Dist, int JunctionId) const {
+	roadmanager::Position::NextJunction next = OdrPosition().GetNextJunction();
+	bool isLess = next.distance < UuToMeters(Dist);
+	if (JunctionId == -1) {
+		return isLess;
+	} else {
+		return isLess && next.junction && next.junction->GetId() == JunctionId;
+	}
+}
+
+float UOpenDrivePosition::SDistanceTo(const UOpenDrivePosition* Other) const {
+	if (!IsValid(Other)) return NAN;
+	roadmanager::PositionDiff diff;
+	roadmanager::Position otherPos = Other->OdrPosition();
+	if (OdrPosition().Delta(&otherPos, diff)) {
+		return MetersToUu(diff.ds);
+	} else {
+		return NAN;
+	}
+}
+
+bool UOpenDrivePosition::Delta(const UOpenDrivePosition* Other, float& Ds, float& Dt, int& DLaneId) const {
+	if (!IsValid(Other)) return false;
+	roadmanager::PositionDiff diff;
+	roadmanager::Position otherPos = Other->OdrPosition();
+	bool ret = OdrPosition().Delta(&otherPos, diff);
+	Ds = MetersToUu(diff.ds);
+	Dt = MetersToUu(diff.dt);
+	DLaneId = diff.dLaneId;
+	return ret;
+}
+
+void UOpenDrivePosition::AlignWithLaneCenter() {
+	SetTrackPosition(GetRoadId(), GetLaneId(), GetS(), 0.f, 0.f);
+}
---
+++ Source/OpenDRIVE/Public/OpenDrivePosition.h
@@ -0,0 +1,160 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CoordTranslate.h"
+#include "OpenDrivePosition.generated.h"
+
+using CoordTranslate::UuToMeters;
+using CoordTranslate::MetersToUu;
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, Blueprintable)
+class OPENDRIVE_API UOpenDrivePosition : public UObject {
+	GENERATED_BODY()
+
+protected:
+	roadmanager::Position _TrackPos;
+	FTransform _InertialPosCache;
+
+	void InvalidateCache();
+
+	void SetTrackPosition(const roadmanager::Position& p);
+
+public:
+	UOpenDrivePosition();
+
+	/**
+	* Returns a copy of the wrapped position object
+	*/
+	virtual roadmanager::Position OdrPosition() const;
+
+	/**
+	* Computes the current OpenDRIVE position based on the input transform
+	*/
+	UFUNCTION(BlueprintCallable, CallInEditor, meta = (Category = "OpenDRIVE"))
+	void SetTransform(const FTransform &T);
+
+	/**
+	* Get the transform based on the current OpenDRIVE position
+	*/
+	UFUNCTION(BlueprintCallable, CallInEditor, meta = (Category = "OpenDRIVE"))
+	FTransform GetTransform() const;
+
+	/**
+	* Sets the OpenDRIVE position
+	* @param TrackId Track (road) ID
+	* @param LaneId  Lane ID
+	* @param S       S-coordinate (cm) along the road
+	* @param Offset  Lateral offset (cm) from the lane center
+	* @param H       Heading offset (°) from the lane orientation
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	void SetTrackPosition(int TrackId, int LaneId, float S, float Offset, float H);
+
+	/**
+	* Moves the object along its S coordinate
+	* @param  S       S-coordinate offset (cm)
+	* @param Strategy Strategy to follow in case of junctions (-1=left, 0=forward, 1=right)
+	* @return Success
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	bool MoveAlongS(float S, int Strategy=0);
+
+	/**
+	* Resets the OpenDRIVE position buffer, in case the object was moved away from its previous position
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	void ResetPosition();
+
+	/**
+	* Returns the current Road ID
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	int GetRoadId() const;
+
+	/**
+	* Returns the current Lane ID
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	int GetLaneId() const;
+
+	/**
+	* Returns the current S-coordinate (cm)
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	float GetS() const;
+
+	/**
+	* Returns the current lateral offset (cm) from the lane center
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	float GetT() const;
+
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	void SetT(float T);
+
+	/**
+	* Returns the current heading offset (°) from the lane orientation
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	float GetH() const;
+
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	void SetH(float H);
+
+	/**
+	* Returns the width of the lane at the current position
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	float GetLaneWidth() const;
+
+	/**
+	* Returns the current Junction ID, or -1 if N/A
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	int GetJunctionId() const;
+
+	/**
+	* Returns the distance (cm) to the next junction
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	float GetNextJunctionDistance() const;
+
+	/**
+	* Returns the next junction's ID
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	int GetNextJunctionId() const;
+
+	/**
+	* Returns whether the distance to a junction is less than a given distance
+	* @param  Dist       Distance (cm) threshold
+	* @param  JunctionId Junction Id to measure distance to, or -1 to use the next junction
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	bool IsJunctionDistanceLessThan(float Dist, int JunctionId = -1) const;
+
+	/**
+	* Returns the longitudinal distance (cm, also known as "pk" or "S" distance) to another object
+	* @param Other Other object
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	float SDistanceTo(const UOpenDrivePosition* Other) const;
+
+	/**
+	* Returns the delta to the other object
+	* @param Other Other object
+	* @param Ds Delta in S coordinate
+	* @param Dt Delta in T coordinate
+	* @param DLaneId Delta in lane ID
+	* @return Whether the delta was resolved
+	*/
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	bool Delta(const UOpenDrivePosition* Other, float& Ds, float& Dt, int& DLaneId) const;
+
+	UFUNCTION(BlueprintCallable, meta = (Category = "OpenDRIVE"))
+	void AlignWithLaneCenter();
+};
