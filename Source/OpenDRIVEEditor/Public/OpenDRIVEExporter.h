bddf0d928f812d6b63ae55cb77ea6777b056fe27
VITA Mateo <vita@ad.ifsttar.fr>
2024-12-05 09:42:18 +0100

    modified OpenDriveExporter class name

--- Source/OpenDRIVEEditor/Public/OpenDRIVEExporter.h
+++ Source/OpenDRIVEEditor/Public/OpenDRIVEExporter.h
@@ -8,11 +8,11 @@
  * Overrides default export command for .xodr files export support
  */
 UCLASS()
-class OPENDRIVEEDITOR_API UOpenDRIVEExporter : public UExporter
+class OPENDRIVEEDITOR_API UOpenDriveExporter : public UExporter
 {
 	GENERATED_BODY()
 	
-	UOpenDRIVEExporter(const FObjectInitializer& ObjectInitiliazer);
+	UOpenDriveExporter(const FObjectInitializer& ObjectInitiliazer);
 
 	// UExporter interface 
 	virtual bool ExportText(const FExportObjectInnerContext* Context, UObject* Object, const TCHAR* Type, FOutputDevice& Ar, FFeedbackContext* Warn, uint32 PortFlags) override;
